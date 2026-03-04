5cc5cb4684837279f1e9f3e9dbb7918cd7321990
aldaniil <alkhanov@t8.ru>
2026-03-04 19:00:26 +0300

    Add logging and fix error messages

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,35 @@
+import { errors } from 'celebrate';
 import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
 
-import NotFoundError from './exceptions/not-found'
-import errorHandler from './middlewares/error-handler'
-import orderRouter from './routes/order'
-import productRouter from './routes/products'
+import NotFoundError from './exceptions/not-found';
+import errorHandler from './middlewares/error-handler';
+import { errorLogger, requestLogger } from './middlewares/logger';
+import orderRouter from './routes/order';
+import productRouter from './routes/products';
 
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek' } = process.env;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(errorHandler);
+app.use(requestLogger);
 
-mongoose.connect('mongodb://127.0.0.1:27017/weblarek');
+mongoose.connect(DB_ADDRESS);
 
 app.use('/product', productRouter);
 app.use('/order', orderRouter);
 app.use('*', (_req, _res, next) => {
-  next(new NotFoundError('Указанный адрес не существует'))
+  next(new NotFoundError('Указанный адрес не существует'));
 })
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Express started on port ${PORT}`);
